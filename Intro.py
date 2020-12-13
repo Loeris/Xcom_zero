2423b56366e83ef5afa20bc9966be6195c1a2fcc
Loeris <fly.rev1ng@gmail.com>
2020-12-13 17:41:06 +0300

    Changed: Intro optimized

--- Intro.py
+++ Intro.py
@@ -10,22 +10,24 @@ def intro(screen):
     s = [(random.random() * width, random.random() * height, 1, 1) for _ in range(stars)]
     if True:
         color = pygame.Color(255, 255, 100)
-        for i in s:
-            screen.fill(pygame.Color('white'),
-                        i)
+        for i in range(stars // 2):
+            screen.fill(pygame.Color('white'), s[2 * i])
+            screen.fill(pygame.Color('white'), s[2 * i + 1])
             pygame.draw.circle(screen, (0, 0, 0), (width // 2, height // 2), border * 9)
             pygame.display.flip()
         for i in range(border * 2 + 1):
             pygame.draw.circle(screen, color, (width // 2, height // 2), i)
             pygame.display.flip()
-            pygame.time.wait(10)
+            pygame.time.wait(1)
         for i in range(border * 8 + 1):
             pygame.draw.circle(screen, color, (width // 2, height // 2), border * 2 + i)
             pygame.draw.circle(screen, (0, 0, 0), (width // 2, height // 2), border + i)
             pygame.draw.circle(screen, color, (width // 2, height // 2), border * 2)
             pygame.display.flip()
-            pygame.time.wait(10)
-        for i in range(border * 2, height // 2):
+            pygame.time.wait(1)
+        for i in range(border * 7, height // 2):
+            for j in range(stars // 4):
+                screen.fill(pygame.Color('white'), s[j])
             pygame.draw.line(screen, color, (width // 2 - 2 * i, height // 2 - i),
                              (width // 2 + 2 * i, height // 2 + i), border)
             pygame.draw.line(screen, color, (width // 2 - 2 * i, height // 2 + i),
@@ -38,17 +40,24 @@ def intro(screen):
             pygame.draw.circle(screen, (0, 0, 0), (width // 2, height // 2), border * 9)  # O
             pygame.draw.circle(screen, color, (width // 2, height // 2), border * 2)  # O
             pygame.display.flip()
-            pygame.time.wait(1)
+            # pygame.time.wait(1)
+        pygame.time.wait(300)
         for i in range(height // 2):
-            pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(width // 2 - 2 * i, height // 2 - i,
-                                                            4 * i, 2 * i))  # Прорезь
-            for j in s:
-                screen.fill(pygame.Color('white'),
-                            j)
+            for j in range(stars // 4):
+                screen.fill(pygame.Color('white'), s[j])
+            pygame.draw.line(screen, (0, 0, 0), (width // 2 - 2 * i, height // 2 - i),
+                             (width // 2 + 2 * i, height // 2 + i), border)
+            pygame.draw.line(screen, (0, 0, 0), (width // 2 - 2 * i, height // 2 + i),
+                             (width // 2 + 2 * i, height // 2 - i), border)
+            pygame.draw.line(screen, (0, 0, 0), (width // 2, height // 2 - i),
+                             (width // 2, height // 2 + i), border)
+            pygame.draw.line(screen, (0, 0, 0), (width // 2 - 2 * i, height // 2),
+                             (width // 2 + 2 * i, height // 2), border)
             pygame.draw.circle(screen, color, (width // 2, height // 2), border * 10)  # O
             pygame.draw.circle(screen, (0, 0, 0), (width // 2, height // 2), border * 9)  # O
             pygame.draw.circle(screen, color, (width // 2, height // 2), border * 2)  # O
             pygame.display.flip()
+
         hsv = color.hsva
         for i in range(100):
             color.hsva = (hsv[0], hsv[1], i, hsv[3])
@@ -74,11 +83,11 @@ def intro(screen):
                              (width // 2 + border * 40, height // 2 + border * 10 - border // 2), border)  # E
             pygame.draw.line(screen, color, (width // 2 + border * 30, height // 2),
                              (width // 2 + border * 40, height // 2), border)  # E
-            pygame.draw.line(screen, color, (width // 2 + border * 30, height // 2 - border * 10 + border // 2),
-                             (width // 2 + border * 40, height // 2 - border * 10 + border // 2), border)  # E
+            pygame.draw.line(screen, color, (width // 2 + border * 30, height // 2 - border * 10 + border // 2 - 1),
+                             (width // 2 + border * 40, height // 2 - border * 10 + border // 2 - 1), border)  # E
             pygame.display.flip()
             pygame.time.wait(10)
-        pygame.time.wait(50)
+        pygame.time.wait(400)
         for i in range(100):
             color.hsva = (hsv[0], hsv[1], 100 - i, hsv[3])
             pygame.draw.line(screen, color, (width // 2 - border * 34, height // 2 - border * 10),
@@ -106,15 +115,16 @@ def intro(screen):
                              (width // 2 + border * 40, height // 2 + border * 10 - border // 2), border)  # E
             pygame.draw.line(screen, color, (width // 2 + border * 30, height // 2),
                              (width // 2 + border * 40, height // 2), border)  # E
-            pygame.draw.line(screen, color, (width // 2 + border * 30, height // 2 - border * 10 + border // 2),
-                             (width // 2 + border * 40, height // 2 - border * 10 + border // 2), border)  # E
+            pygame.draw.line(screen, color, (width // 2 + border * 30, height // 2 - border * 10 + border // 2 - 1),
+                             (width // 2 + border * 40, height // 2 - border * 10 + border // 2 - 1), border)  # E
             pygame.display.flip()
             pygame.time.wait(15)
-        for i in s:
-            screen.fill(pygame.Color('black'),
-                        i)
+        for i in range(stars // 3):
+            screen.fill(pygame.Color('black'), s[3 * i])
+            screen.fill(pygame.Color('black'), s[3 * i + 1])
+            screen.fill(pygame.Color('black'), s[3 * i + 2])
             pygame.display.flip()
         for i in range(255):
             screen.fill((i, i, i))
             pygame.display.flip()
-            pygame.time.wait(7)
+            pygame.time.wait(5)
