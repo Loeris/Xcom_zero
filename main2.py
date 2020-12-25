25746a6690c931097a0147b90c3c07c6ce8d8105
Yana <yana.grin.2019@mail.ru>
2020-12-25 21:09:02 +0300

    "начало структурирования"

--- main2.py
+++ main2.py
@@ -56,11 +56,26 @@ class Table(Menu):
         super(Table, self).__init__(w, h, le, t)
         self.color = "red"
 
+class Robot:
+    def __init__(self, position, size):
+        self.size = size
+        self.position = position
+        self.core = Rect((position[0]+2, position[1]+2), (size[0]-4, size[1]-4))
+    
+    def move(self, target):
+        pass
+
 
 class Desk(Menu):
     def __init__(self, w, h, le, t):
         super(Desk, self).__init__(w, h, le, t)
         self.current_cell = None
+
+    def get_cell_by_point(self, pos):#возвращает ячейку массива по координатам клика
+        pass
+
+    def get_point_by_cell(self, cell):
+        pass
 
     def on_click(self, cell_cords):
         cell = self.data[cell_cords[1]][cell_cords[0]]
@@ -123,39 +138,86 @@ class Desk(Menu):
         render_list.append((sprite, left2, top2, cell2))
 
 
+class Robot_controller:
+    def __init__(self, desk, robot_render, logic):
+        self.desk = desk
+        self.robot_render = robot_render
+        self.logic = logic
+        self.selected_robot = None
+        self.path=[]
+        self.point = None
+
+    def click(self, click_pos):
+        if len(self.path)>0:
+            return
+        cell = self.desk.get_cell_by_point(click_pos)
+        if cell and self.selected_robot==None:
+            self.select_robot(cell)
+        elif cell and self.selected_robot and self.logic.canMove(cell, self.selected_robot):
+            self.path = self.logic.move(self.selected_robot,cell)
+
+    def select_robot(self, cell):
+        self.selected_robot = self.logic.get_robot(coord)
+
+    def update(self, screen):
+        if self.selected_robot==None:
+            return
+
+        if len(path)==0 and self.point != None:
+            self.selected_robot=None
+        
+        if self.desk.get_cell_by_point(self.point) == self.path[0]:
+            self.point = None
+
+        if self.point:
+            self.robot_render.render_move_to(screen, self.point, self.selected_robot)
+        elif len(path)>0 and self.point==None:
+            self.point = get_point_by_cell(path.pop(0))
+            
+
+
+
 if __name__ == '__main__':
     pygame.init()
     render_list = []
-    size = width, height = 1000, 500
+    size = width,height = 1000, 500
     screen = pygame.display.set_mode(size)
     border = height // 80
     # intro(screen)
     desk = Desk(12, 8, border * 8, border * 2)
     table = Table(2, 2, border * 138, border * 2)
     running = True
-    cell0 = desk.data[5][7]
-    render_list.append((sprite_alien, cell0[1][0], cell0[1][1], cell0))
-    cell0 = desk.data[1][7]
-    render_list.append((sprite_heavy, cell0[1][0], cell0[1][1], cell0))
-    cell0 = desk.data[1][4]
-    render_list.append((sprite_sniper, cell0[1][0], cell0[1][1], cell0))
-    cell0 = desk.data[3][8]
-    render_list.append((sprite_ranger, cell0[1][0], cell0[1][1], cell0))
-    cell0 = desk.data[3][3]
-    render_list.append((sprite_support, cell0[1][0], cell0[1][1], cell0))
-    cell0 = desk.data[5][4]
-    render_list.append((sprite_common, cell0[1][0], cell0[1][1], cell0))
+
+    r_controller = Robot_controller(desk, None, None)
+
+    # cell0 = desk.data[8][8]
+    # render_list.append((sprite_alien, cell0[1][0], cell0[1][1], cell0))
+    # cell0 = desk.data[1][7]
+    # render_list.append((sprite_heavy, cell0[1][0], cell0[1][1], cell0))
+    # cell0 = desk.data[1][4]
+    # render_list.append((sprite_sniper, cell0[1][0], cell0[1][1], cell0))
+    # cell0 = desk.data[3][8]
+    # render_list.append((sprite_ranger, cell0[1][0], cell0[1][1], cell0))
+    # cell0 = desk.data[3][3]
+    # render_list.append((sprite_support, cell0[1][0], cell0[1][1], cell0))
+    # cell0 = desk.data[8][8]
+    # render_list.append((sprite_common, cell0[1][0], cell0[1][1], cell0))
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                desk.get_click(event.pos)
+                r_controller.click(event.pos)
+                #desk.get_click(event.pos)
         screen.fill((255, 255, 255))  # цвет экрана
-        for func, left, top, cell in render_list:
-            func(screen, left, top)
+
+        # for func, left, top, cell in render_list:
+        #     func(screen, left, top)
+
         desk.render(screen)
         table.render(screen)
+        r_controller.update(screen)
+
         pygame.display.flip()
     pygame.quit()
